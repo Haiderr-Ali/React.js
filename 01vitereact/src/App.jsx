cdce91844608a95672bcc129c749e7fd39bead01
Haider Ali <alihaiderali1050@gmail.com>
2024-05-17 00:11:12 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./haider"
+function App() {
+ 
+
+  return (
+   <>
+   <Chai/>
+   <h1>Haider Ali</h1>
+   <p>Learning React.js</p>
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/haider.jsx
@@ -0,0 +1,6 @@
+function Chai() {
+    return(
+        <h3>My Name Is Haider Ali.</h3>
+    )
+}
+export default Chai
