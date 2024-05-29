bbb31a9f75096567cc93bb6e21d6bebc02bfa89c
Haider Ali <alihaiderali1050@gmail.com>
2024-05-29 13:49:50 +0530

    update profile.jsx

--- minicontextapi/src/components/Profile.jsx
+++ minicontextapi/src/components/Profile.jsx
@@ -3,6 +3,7 @@ import UserContext from '../Context/UserContext'
 function Profile() {
     const {user} = useContext(UserContext)
   if (!user) return <div>Please Login </div>
+  
   return <div>Welcome {user.username}!</div>
 }
 
